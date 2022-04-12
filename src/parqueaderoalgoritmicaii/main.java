c8382c975c763de81ac0d41d79223461ab4de5f4
Axel <Axel@192.168.1.12>
2022-04-12 11:32:26 -0500

    actualizado Ultimate

--- src/parqueaderoalgoritmicaii/main.java
+++ src/parqueaderoalgoritmicaii/main.java
@@ -26,6 +26,7 @@ public class main {
         System.out.println("Hola soy Richard");
         System.out.println("Ya me salio creo a");
         System.out.println("Clonacion Ysaac Correa");
+        System.out.println("Si al parecer ya te salio Ysaac :v");
     }
     
 }
