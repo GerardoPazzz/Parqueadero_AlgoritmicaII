43c465989c4dc7a29cec9ef7b3bbd871cbc12a0e
Axel <Axel@192.168.1.12>
2022-04-12 21:34:41 -0500

    actualizado nueva clase

---
+++ src/Parquedero/Clases/Parqueo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Parquedero.Clases;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Axel
+ */
+public class Parqueo {
+    private int id;
+    private Date HoraDeEntrada;
+    
+}
