646b6e04c131484ee150b88737f35b7980fa980b
Axel <Axel@192.168.1.12>
2022-04-13 12:06:22 -0500

    Axel(clase parqueo)

--- src/Parquedero/Clases/Parqueo.java
+++ src/Parquedero/Clases/Parqueo.java
@@ -13,5 +13,45 @@ import java.util.Date;
 public class Parqueo {
     private int id;
     private Date HoraDeEntrada;
+    private long cont1,cont2;
     private Date HoraDeSalida;
+    private Cliente propietario;
+    
+    public Parqueo(int id,Cliente propietario){
+        HoraDeEntrada=new Date();
+        cont1=System.currentTimeMillis();
+        this.id=id;
+        this.propietario=propietario;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPropietario(Cliente propietario) {
+        this.propietario = propietario;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getHoraDeEntrada() {
+        return HoraDeEntrada;
+    }
+
+    public Date getHoraDeSalida() {
+        return HoraDeSalida;
+    }
+
+    public Cliente getPropietario() {
+        return propietario;
+    }
+    
+    public int getTiempoTotal(){
+        HoraDeSalida=new Date();
+        cont2=System.currentTimeMillis();
+        int x=(int)((cont2-cont1)/3600000);
+        return x;
+    }
 }
